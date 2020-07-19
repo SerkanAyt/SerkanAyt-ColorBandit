70c38eb61286e9b3a036a6d175486990b50bafd1
borabafli <borabafli@gmail.com>
2020-07-19 11:54:31 +0300

    ssssss
    ss

--- Gonderilecek Color Bandit/Assets/Codes/reklam.cs
+++ Gonderilecek Color Bandit/Assets/Codes/reklam.cs
@@ -1,68 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using GoogleMobileAds.Api;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using GoogleMobileAds.Api;
 
-public class reklam : MonoBehaviour
-{
-    InterstitialAd interstitial;
+//public class reklam : MonoBehaviour
+//{
+//    InterstitialAd interstitial;
 
-    static reklam reklamKontrol;
-    void Start()
+//    static reklam reklamKontrol;
+//    private string adUnitID;
 
-    {
-        if(reklamKontrol==null)
+//    void Start()
 
-        {
-            DontDestroyOnLoad(gameObject);
-            reklamKontrol = this;
+//    {
+//        if(reklamKontrol==null)
 
-
-            //1. aşama----------------------------------
-#if UNITY_ANDROID
-            string appID = "ca-app-pub-5881142327850120~9950362933";
-#elif UNITY_IPHONE
-        string appID="";
-#else
-        string appID="unexpected_platform";
-#endif
-            MobileAds.Initialize(appID);
-
-            //2. aşama ------------------------------
-#if UNITY_ANDROID
-            string adUnitID = "ca-app-pub-39402560999425544/1033173712";
-#elif UNITY_IPHONE
-        string adunitID="";
-#else
-        string adunitID="unexpected_platform";
-#endif
-
-            interstitial = new InterstitialAd(adUnitID);
-
-            //3.aşama---------------------------------
-
-            AdRequest request = new AdRequest.Builder().AddTestDevice(AdRequest.TestDeviceSimulator).AddTestDevice("2077ef9a63d2b398840261c8221a0c9b").Build();
-            interstitial.LoadAd(request);
-
-            //4.aşama-----------------------------------
+//        {
+//            DontDestroyOnLoad(gameObject);
+//            reklamKontrol = this;
 
 
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+//            //1. aşama----------------------------------
+//#if UNITY_ANDROID
+//            string appID = "ca-app-pub-5881142327850120~9950362933";
+//#elif UNITY_IPHONE
+//        string appID="";
+//#else
+//        string appID="unexpected_platform";
+//#endif
+//            MobileAds.Initialize(appID);
+
+//            //2. aşama ------------------------------
+//#if UNITY_ANDROID
+//            string adUnitID = "ca-app-pub-39402560999425544/1033173712";
+//#elif UNITY_IPHONE
+//        string adunitID="";
+//#else
+//        string adunitID="unexpected_platform";
+//#endif
+
+//            interstitial = new InterstitialAd(adUnitID);
+
+//            //3.aşama---------------------------------
+
+//            AdRequest request = new AdRequest.Builder().AddTestDevice(AdRequest.TestDeviceSimulator).AddTestDevice("2077ef9a63d2b398840261c8221a0c9b").Build();
+//            interstitial.LoadAd(request);
+
+//            //4.aşama-----------------------------------
+
+
+//        }
+//        else
+//        {
+//            Destroy(gameObject);
+//        }
 
 
 
-    }
+//    }
 
-    public void reklamiGoster()
-    {
-        if (interstitial.IsLoaded())
-        {
-            interstitial.Show();
-        }
+//    public void reklamiGoster()
+//    {
+//        if (interstitial.IsLoaded())
+//        {
+//            interstitial.Show();
+//        }
 
-    }
-}
+//    }
+//}
